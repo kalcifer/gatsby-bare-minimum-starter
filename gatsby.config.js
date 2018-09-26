8037c746ada78fc1c7d28df4bdc88a6212615c34
pksjce <pksjce@gmail.com>
2018-09-26 20:36:35 +1000

    Initial commit

---
+++ gatsby.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby 2.0 minimal template `,
+    siteUrl: `https://www.gatsbyjs.org`,
+    description: `Blazing fast modern site generator for React`
+  },
+  plugins: []
+};
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default () => <div>Landing page</div>;
